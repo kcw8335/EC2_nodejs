9675f89289438541e179e64be121d482d65d98f4
kimchangwoo <kcw8335@gmail.com>
2020-11-17 12:13:52 +0000

    1117

--- send_file/function.py
+++ send_file/function.py
@@ -13,78 +13,43 @@ def parsing_XML_control(filename):
     occasion = root.findtext("occasion")
     return [edgeNo, traffic_light, how_many, occasion]
 
-def occasion_decision(current_occasion, occasion):
-    # 현재 상황이 정상, 제어신호는 사고 대응
-    # 정상상황에서 사고대응상황으로 변경
-    if current_occasion == "NA" and occasion == "A":
-        # 현재 상황을 사고 상황으로 돌리기
-        return ["A", True, "정상 => 사고"]
-    # 현재 상황이 사고 대응, 제어신호는 정상화
-    # 사고상황에서 정상상황으로 변경
-    elif current_occasion == "A" and occasion == "normalization":
-        # 현재 상황을 정상으로 돌리기
-        return ["NA", True, "사고 => 정상"]
-    # 현재 상황이 정상, 제어 신호 정상
-    # 정책변경 상황
-    elif current_occasion == "NA" and occasion == "NA":
-        # 현재 상황은 정상으로 유지
-        return ["NA", True, "정책변경"]
-    # 나머지 신호에 대해서는 거부
-    else:
-        return [current_occasion, False]
-
-# ===================================================================================================================================================
-
 # pip install pymysql
 import pymysql
 
 # 어떤 control.xml 데이터가 들어왔는 지에 대한 로그
 # 모든 control.xml 데이터를 로깅
-def control_xml_logging(tmp_list):
-    edgeNo = tmp_list[0]
-    traffic_light = tmp_list[1]
-    how_many = tmp_list[2]
-    occasion = tmp_list[3]
+def control_xml_logging(control_list, db_list):
+    # control 정보 가져오기
+    edgeNo = control_list[0]
+    traffic_light = control_list[1]
+    how_many = control_list[2]
+    occasion = control_list[3]
+
+    # db 계정 정보 가져오기
+    host = db_list[0]
+    user = db_list[1]
+    password = db_list[2]
+    db = db_list[3]
+
     # MySQL Connection 연결
-    conn = pymysql.connect(host='database-1.ckkbkqdct7py.ap-northeast-2.rds.amazonaws.com', user='admin', password='kshieldjr', db='project', charset='utf8')
+    conn = pymysql.connect(host=host, user=user, password=password, db=db, charset='utf8')
     # Connection으로부터 Cursor 생성
     curs = conn.cursor()
+    # 모든 xml 데이터 로깅
     sql = "insert into Control_Xml_Log(edgeNo, traffic_light, how_many, occasion, logging_date) values(%s, %s, %s, %s, now())"
     curs.execute(sql, (edgeNo, traffic_light, how_many, occasion))
-    conn.commit()
-    conn.close()
     
-# control.xml을 보내기 전에 데이터베이스에 로깅하기 위한 함수
-# NA_Log, A_Log
-def logging(tmp_list, state_change):
-    edgeNo = tmp_list[0]
-    traffic_light = tmp_list[1]
-    how_many = tmp_list[2]
-    occasion = tmp_list[3]
-    # MySQL Connection 연결
-    conn = pymysql.connect(host='database-1.ckkbkqdct7py.ap-northeast-2.rds.amazonaws.com', user='admin', password='kshieldjr', db='project', charset='utf8')
-    # Connection으로부터 Cursor 생성
-    curs = conn.cursor()
-    # SQL문 만들기
-    # 정책 변경시 로그 기록
-    if state_change == "정책변경":
+    if occasion == "NA":
+        state_change = "정책변경"
         sql = "insert into NA_Log(state_change, edgeNo, traffic_light, how_many, logging_date) values(%s, %s, %s, %s, now())"
         curs.execute(sql, (state_change, edgeNo, traffic_light, how_many))
-        conn.commit()
-        conn.close()
-    # 정상 => 사고 상황 로그 기록
-    elif state_change == "정상 => 사고":
+    elif occasion == "A":
+        state_change = "사고대응"
         sql = "insert into A_Log(state_change, edgeNo, traffic_light, how_many, logging_date) values(%s, %s, %s, %s, now())"
         curs.execute(sql, (state_change, edgeNo, traffic_light, how_many))
-        conn.commit()
-        conn.close()
-    # 사고 => 정상 상황 로그 기록
-    elif state_change == "사고 => 정상":
+    elif occasion == "nomalization":
+        state_change = "정상화"
         sql = "insert into A_Log(state_change, edgeNo, traffic_light, how_many, logging_date) values(%s, %s, %s, %s, now())"
-        curs.execute(sql, (state_change, "all_reset", "all_reset", "all_reset"))
-        conn.commit()
-        conn.close()
-
-
-
-
+        curs.execute(sql, (state_change, edgeNo, "all_reset", 0))
+    conn.commit()
+    conn.close()
--- send_file/send_control_xml.py
+++ send_file/send_control_xml.py
@@ -4,27 +4,29 @@ import os
 
 import json # import json module
 
-# with statement
-with open('instruction.json', encoding="UTF-8") as json_file:
+# secrets.json 파일 열어서 데이터 받아오기
+with open('secrets.json', encoding="UTF-8") as json_file:
     json_data = json.load(json_file)
 
 while True:
     # 파일이 있다면 상황판단 후 데이터베이스에 로깅 후 전송
     if os.path.isfile(json_data["file_path_xml"]):
         # control.xml 파싱하기
-        tmp_list = parsing_XML_control(json_data["file_path_xml"])
-       
+        control_list = parsing_XML_control(json_data["file_path_xml"]) 
+        # json에서 디비 정보 가져오기
+        db_list = json_data["db"]
+
         # 모든 xml 데이터를 데이터베이스에 로깅
-        control_xml_logging(tmp_list)
+        control_xml_logging(control_list, db_list)
         
         # 엣지 1로 보내기 위한 코드
-        if tmp_list[0] == "1":
+        if control_list[0] == "1":
             os.system(json_data["mv_1"])
             # 관리자 페이지에서 생성된 xml 파일을 라파통신 폴더로 넘겨주기
             os.system(json_data["instruction_1"])
             os.remove(json_data["file_path_xml_1"])
         # 엣지 2로 보내기 위한 코드
-        elif tmp_list[0] == "2":
+        elif control_list[0] == "2":
             os.system(json_data["mv_2"])
             # 관리자 페이지에서 생성된 xml 파일을 라파통신 폴더로 넘겨주기
             os.system(json_data["instruction_2"])
